4466a3f01c5925120f65d59c8727c5e376a099e3
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-02-29 03:04:49 +0530

    add 3 book shop main page + Save Controller

--- src/main/java/com/example/bookshop/controller/BookController.java
+++ src/main/java/com/example/bookshop/controller/BookController.java
@@ -1,10 +1,17 @@
 package com.example.bookshop.controller;
 
+import com.example.bookshop.model.Book;
+import com.example.bookshop.repository.BookRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@AllArgsConstructor
 public class BookController {
+    private final BookRepository bookRepository;
     @GetMapping("/")
     public String home (){
         return "home";
@@ -13,8 +20,14 @@ public class BookController {
     public String book_register(){
         return "bookRegister";
     }
+    @PostMapping("/save")
+    public String saveBook(Book book){
+        bookRepository.save(book);
+        return "redirect:book_register";
+    }
     @GetMapping("/available_book")
-    public String  getAllBook(){
+    public String  getAllBook(Model model){
+        model.addAttribute("book", bookRepository.findAll());
         return "bookList";
     }
 }
