5bef29eefda83507abd1990b225b12d6a857c497
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-02-28 05:33:53 +0530

    add model Book

--- src/main/java/com/example/bookshop/controller/BookController.java
+++ src/main/java/com/example/bookshop/controller/BookController.java
@@ -1,2 +1,12 @@
-package com.example.bookshop.controller;public class BookController {
+package com.example.bookshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+    @GetMapping("/")
+    public String home (){
+        return "home";
+    }
 }
