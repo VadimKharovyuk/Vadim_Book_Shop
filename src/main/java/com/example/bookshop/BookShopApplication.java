491d4ce4bcb253ce29b7ba4f3fb40281a4897699
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-03-08 04:31:36 +0530

    Чат + титулка

--- src/main/java/com/example/bookshop/BookShopApplication.java
+++ src/main/java/com/example/bookshop/BookShopApplication.java
@@ -8,6 +8,8 @@ public class BookShopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookShopApplication.class, args);
+
+//		 <script src="https://kit.fontawesome.com/0faddc1af8.js" crossorigin="anonymous"></script>-->
 	}
 
 }
